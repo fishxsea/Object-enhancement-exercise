c666eb4f8c742f6282afd8661d6ed5eb867b3563
Aaron Clayton <fish@sea.local>
2023-08-18 05:43:22 -0500

    first commit

---
+++ obj-enhancements.js
@@ -0,0 +1,84 @@
+// 1. Same keys and values
+
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName: firstName,
+//       lastName: lastName
+//     }
+// }
+
+
+
+function createInstructor(firstName, lastName){
+return {
+    firstName,
+    lastName
+    }
+}
+console.log(createInstructor('aaron', 'clayton'))
+
+// -----------------------------------------------------
+
+// 2. Computed Property Names
+
+// var favoriteNumber = 42;
+// var instructor = {
+//   firstName: "Colt"
+// }
+// instructor[favoriteNumber] = "That is my favorite!"
+
+const favoriteNumber = 42;
+const instructor = {
+    firstName: 'Colt',
+    [favoriteNumber]: 'That is my favorite!'
+}
+
+// -----------------------------------------------------
+
+// 3. Object Methods
+
+// var teacher = {
+//   firstName: "Colt",
+//   sayHi: function(){
+//     return "Hi!";
+//   },
+//   sayBye: function(){
+//     return this.firstName + " says bye!";
+//   }
+// }
+
+var teacher = {
+    firstName: "Colt",
+    sayHi(){
+      return "Hi!";
+    },
+
+    sayBye(){
+      return this.firstName + " says bye!";
+    }
+  }
+
+
+// -----------------------------------------------------
+ 
+ // 4. createAnimal function
+
+//  const d = createAnimal("dog", "bark", "Woooof!")
+// // {species: "dog", bark: ƒ}
+// d.bark()  //"Woooof!"
+
+// const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+// // {species: "sheep", bleet: ƒ}
+// s.bleet() //"BAAAAaaaa"
+
+function createAnimal(species, action, sound){
+    return {
+        species,
+        [action](){
+            return sound
+        }
+    }
+}
+
+const lion = createAnimal('lion', 'roar', 'meow')
+console.log(lion.roar())
